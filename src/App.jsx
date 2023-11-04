e37c9388e16e88b9e098c16446b89791e18bd2f0
helmyyy <92864852+helmyfadlail@users.noreply.github.com>
2023-11-04 19:45:45 +0700

    adding map for content

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ import Vannesa from "../src/assets/vannesa.jpg";
 
 import "./App.css";
 
+const contentList = [
+  {image: Vannesa, name:"Someone", job:"Creative", desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."},
+  {image: Gitta, name:"Someone", job:"Developer", desc:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."},
+  {image: Vannesa, name:"Someone", job:"Product Manager", desc:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."},
+  {image: Gitta, name:"Someone", job:"Developer", desc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."},
+]
+
 // import required modules
 import { EffectCoverflow, Pagination } from "swiper";
 
@@ -43,166 +50,35 @@ const Review = () => {
             modules={[EffectCoverflow, Pagination]}
             className="mySwiper"
           >
-            <SwiperSlide>
-              <div
-                className="testimonialBox "
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: "column",
-                }}
-              >
-                <div>
-                  <img src={quote} alt="tes" style={{ width: "10%" }} />
-                  <div className="content">
-                    <p className="">
-                      Menurut gw, Yovin tuh orangnya keren banget 😎. Yovin itu
-                      orangnya sangat amat pekerja keras dan bertanggung jawab
-                      sama semua kerjaannya, baik akademis atau yang non
-                      akademis. Gw sangat salut sama stress managementnya, kok
-                      bisa tetep professional dan fokus pas lagi ada masalah ini
-                      dan itu?!! Definitely one of the strongest women I have
-                      ever met. Abiss itu, kok bisa se extrovert ituu?!! Social
-                      energynya ga abiss2 hahahaha. Bestt deh pokoknya 🤙🤙
-                    </p>
-                  </div>
-                </div>
-                <div className="details">
-                  <div className="imgBx">
-                    <img src={Vannesa} alt="tes" />
-                  </div>
-                  <h3>
-                    someone
-                    <br />
-                    <span>Creative</span>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div
-                className="testimonialBox "
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: "column",
-                }}
-              >
-                <div>
-                  <img src={quote} alt="tes" style={{ width: "10%" }} />
-                  <div className="content">
-                    <p className="">
-                      kerja sama yovin itu super duper seru dan menantang karena
-                      dia produktif banget, orang yang super tanggung jawab sama
-                      tugasnya. she puts so much efforts for everything she
-                      does, even at her lowest point. jiwanya kerja keras banget
-                      walaupun kadang kepalanya juga ikutan keras alias batu.
-                      but deep down, gua tau dia peduli sama orang orang di
-                      sekitarnya dan mentingin orang lain di atas kepentingan
-                      dia juga. panjang umur deh ya orang baik, stay positive
-                      and gawl😋
-                    </p>
-                  </div>
-                </div>
-                <div className="details">
-                  <div className="imgBx">
-                    <img src={Gitta} alt="tes" />
-                  </div>
-                  <h3>
-                    someone
-                    <br />
-                    <span>Creative</span>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div
-                className="testimonialBox "
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: "column",
-                }}
-              >
-                <div>
-                  <img src={quote} alt="tes" style={{ width: "10%" }} />
-                  <div className="content">
-                    <p className="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Eaque sed optio recusandae temporibus, eum, ab error ut
-                      nihil quae iste quam repellat quis quisquam culpa animi
-                      eos, voluptatibus ducimus assumenda.
-                    </p>
-                  </div>
-                </div>
-                <div className="details">
-                  <div className="imgBx">
-                    <img src={foto} alt="tes" />
-                  </div>
-                  <h3>
-                    someone
-                    <br />
-                    <span>Creative</span>
-                  </h3>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div
-                className="testimonialBox"
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: "column",
-                }}
-              >
-                <img src={quote} alt="tes" style={{ width: "10%" }} />
-                <div className="content">
-                  <p className="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eaque sed optio recusandae temporibus, eum, ab error ut
-                    nihil quae iste quam repellat quis quisquam culpa animi eos,
-                    voluptatibus ducimus assumenda.
-                  </p>
-                  <div className="details">
-                    <div className="imgBx">
-                      <img src={foto} alt="tes" />
+            {contentList.map((item,index)=>(
+                <SwiperSlide key={index}>
+                  <div
+                    className="testimonialBox "
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <div>
+                      <img src={quote} alt="tes" style={{ width: "10%" }} />
+                      <div className="content">
+                        <p className="">{item.desc}</p>
+                      </div>
                     </div>
-                    <h3>
-                      someone
-                      <br />
-                      <span>Creative</span>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="testimonialBox">
-                <img src={quote} alt="tes" style={{ width: "10%" }} />
-                <div className="content">
-                  <p className="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eaque sed optio recusandae temporibus, eum, ab error ut
-                    nihil quae iste quam repellat quis quisquam culpa animi eos,
-                    voluptatibus ducimus assumenda.
-                  </p>
-                  <div className="details">
-                    <div className="imgBx">
-                      <img src={foto} alt="tes" />
+                    <div className="details">
+                      <div className="imgBx">
+                        <img src={item.image} alt="tes" />
+                      </div>
+                      <h3>
+                        {item.name}
+                        <br />
+                        <span>{item.job}</span>
+                      </h3>
                     </div>
-                    <h3>
-                      someone
-                      <br />
-                      <span>Creative</span>
-                    </h3>
                   </div>
-                </div>
-              </div>
-            </SwiperSlide>
+                </SwiperSlide>
+              ))}
           </Swiper>
         </div>
       </section>
